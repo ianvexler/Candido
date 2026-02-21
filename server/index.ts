17f1c43af7754cf3f64a84d3abd2b7db9e9d72a3
Ian Vexler <ianvexler@Ians-MacBook-Pro.local>
2026-02-21 14:05:12 +0000

    Added Prisma

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,19 @@
-const express = require('express');
-import type { Request, Response } from 'express';
+import "dotenv/config";
+import express from "express";
+import type { Request, Response } from "express";
+import { prisma } from "./lib/prisma.js";
+
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (_req: Request, res: Response) => {
-  res.send('API is running');
+app.get("/", (_req: Request, res: Response) => {
+  res.send("API is running");
 });
 
-app.get('/api/hello', (_req: Request, res: Response) => {
-  res.json({ message: 'Hello world' });
+app.get("/api/users", async (_req: Request, res: Response) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
 });
 
 const PORT = process.env['PORT'] ?? 8000;
